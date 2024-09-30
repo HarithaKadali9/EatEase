e889b34b3262b795b9b9cfbaa950a76d07522084
HarithaKadali9 <kadaliharitha23@gmail.com>
2024-09-30 17:17:09 +0330

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ export default {
       colors: {
         cement: '#2B2B2B'  // Custom cement-like color (light grayish)
       },
-      backgroundImage: { 'food-image': "url('/src/assets/foodie.jpg')", 'food-image-2': "url('src/assets/bgfood.png')", 'food-image-3': "url('src/assets/productphotos/caesarsalad.jpg')", 'food-image-4': "url('src/assets/bgimage.jpg')"}
+      backgroundImage: { 'food-image': "url('my_food_app/public/assets/foodie.jpg')", 'food-image-2': "url('my_food_app/public/assets/bgfood.png')", 'food-image-3': "url('my_food_app/public/assets/productphotos/caesarsalad.jpg')", 'food-image-4': "url('my_food_app/public//assets/bgimage.jpg')"}
     },
   },
   plugins: [],
