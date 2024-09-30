791d37534832be07b5710bf5ae2d5390dca586ac
HarithaKadali9 <kadaliharitha23@gmail.com>
2024-09-30 10:50:56 +0330

    Third commit

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -4,7 +4,7 @@ import notebook from '../assets/notebookL.png'
 
 const Download = () => {
   return (
-    <div className='bg-food-image bg-cover bg-bottom grid grid-cols-2 sm:h-[400px]' id='download'>
+    <div className='bg-food-image-3 bg-cover bg-bottom grid grid-cols-2 sm:h-[400px]' id='download'>
         <div className='relative bottom-0'>
             <img className='absolute w-[350px] z-10 right-[20px] bottom-[10px]' src={mobile} />
             <img className='absolute w-[700px]' src={notebook} />
